acaba61ca5442a15eac991b033725d168bdaf1df
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-04-01 11:45:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function logProduct(a, b) {
 function onRequest(request, response){
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hello world');
-  logProduct(4,5)
+  logProduct(4,5);
   response.end();
 }
 
