da684516545cde330291f854136830bf10189f54
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-04-02 14:53:09 +0200

    html file

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 var http = require('http');
 var multiply = require('./multiply-module');
 var exponentiation = require('./exponentiation-module')
+var fs = require('fs'); // fs = filesystem --> Used to reach HTML files
 
 function onRequest(request, response){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.writeHead(200, {'Content-Type': 'text/html'}); // text/plain if normal text
+  response.write('Hello world');
+
+  //actions with module files
   response.write(multiply.moduleMessage);
   multiply.logProduct(3, 14);
   exponentiation.logExponentiation(7,3);
-  response.end();
+
+  //Launching HTML file
+  fs.readFile('./index.html', null, function(error, data){
+    if (error) {
+      response.writeHead(404);
+      response.write('File not found');
+    } else {
+      response.write(data);
+    }
+    response.end(); // This has to be within the function because Node.js is async!
+  });
 }
 
 http.createServer(onRequest).listen(3000);
